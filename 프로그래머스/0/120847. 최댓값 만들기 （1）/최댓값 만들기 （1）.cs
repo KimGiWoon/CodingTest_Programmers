7fad9a4e24dd1925ac560717b70da1b7617cba79
KGW2 <rldns20704@naver.com>
2025-05-30 23:50:55 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 2.51 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        
+        Array.Sort(numbers);
+        answer = numbers[numbers.Length - 1] * numbers[numbers.Length - 2];        
+        return answer;
+    }
+}

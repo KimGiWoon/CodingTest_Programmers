8db396a450f7d5ef117ba7760cc7b5a0a3acb44c
KGW2 <rldns20704@naver.com>
2025-06-07 23:12:15 +0900

    [level 0] Title: 대문자와 소문자, Time: 3.03 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Linq;
+
+public class Solution {
+    public string solution(string my_string) {
+        string answer = "";
+        
+        var selectResult = my_string.Select(c => char.IsUpper(c) ? char.ToLower(c) : char.ToUpper(c));
+        answer = new String(selectResult.ToArray());
+
+        return answer;
+    }
+}
